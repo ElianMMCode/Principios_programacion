c353781dbe5d74858b0d0f1bc5d957ac2542e2c9
ElianMMCode <elianferney19@gmail.com>
2025-03-10 12:20:17 -0500

    correcion nombre

---
+++ src/Ejrc_17_Porcentaje_descuento.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Ejrc_17_Porcentaje_descuento {
+    public static void main (String[] args){
+            Scanner teclado=new Scanner(System.in);
+            double precio_final;
+            double precio_venta;
+
+
+            System.out.println("Calculo de descuento");
+            System.out.println("=====================");
+            System.out.println("Ingrese el precio pagodo producto");
+            precio_venta= teclado.nextDouble();
+            System.out.println("=====================");
+            System.out.println("Ingrese el precio de venta del producto");
+            precio_final= teclado.nextDouble();
+
+            //Calculo descuento
+
+            if (precio_venta>precio_final){
+
+                double descuento=(precio_final/precio_venta)*100;
+                System.out.println("==============================");
+                System.out.println("DESCUENTO DE "+descuento+"%");
+
+            }else{
+
+                System.out.println("El precio de compra debe ser menor que el valor de venta para calcular el descuento");
+            }
+        }
+    }
