261903c09124bd2ee4a985b52ccc28fb3a5e6cca
ElianMMCode <elianferney19@gmail.com>
2025-03-10 15:33:08 -0500

    Ejercicios primera parte

---
+++ src/Ejrc_20_Interes_hipoteca.java
@@ -0,0 +1,30 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Ejrc_20_Interes_hipoteca {
+    public static void main(String[] args){
+
+        Scanner teclado=new Scanner(System.in);
+
+        System.out.println("CALCULO DE INTERESES HIPOTECA");
+        System.out.println("-----------------------------");
+        System.out.println("Ingrese el valor del prestamo");
+        double prestamo = teclado.nextDouble();
+        System.out.println("Inrgese el valor de los intereses pagados");
+        double pago_intereses = teclado.nextDouble();
+        System.out.println("Ingrese el tiempo (años) pagados");
+        int tiempo = teclado.nextInt();
+
+        //CALCULO DE INTERESES AÑO
+
+        DecimalFormat dc= new DecimalFormat("#");
+        double razon = 100*pago_intereses/prestamo*tiempo;
+
+        System.out.println("Los intereses anuales fueron de: ");
+        System.out.println(dc.format(razon)+"%");
+
+    }
+
+
+
+}
---
+++ src/Ejrc_23_Tonelada_maiz.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Ejrc_23_Tonelada_maiz {
+    public static void main(String[] args){
+        Scanner teclado = new Scanner(System.in);
+
+
+        System.out.println("PRECIO TONELADA MAIZ");
+        System.out.println();
+        System.out.println("<><><><><><><><><><>");
+
+        //Tonelada
+        System.out.println("Ingrese los Kg de harina");
+        double kg= teclado.nextDouble();
+        System.out.println("Ingrese los lt de aceite");
+        double lt= teclado.nextDouble();
+
+        //precio bulto y caja
+        System.out.println("Precio del bulto");
+        double pr_bulto= teclado.nextDouble();
+        int bulto= (int) (((int)(kg/24))*pr_bulto);
+
+        //*pr_bulto);
+        System.out.println("Precio de la caja");
+        int pr_caja = teclado.nextInt();
+        int caja=(((int)(lt/15))*pr_caja);
+
+        //Precio detal
+        System.out.println("Precio al detal del bulto");
+        double prdt_bulto = teclado.nextDouble();
+        int dt_bulto = (int) (((int)(kg%24))*prdt_bulto);
+
+        System.out.println("Precio al detal de la caja");
+        double prdt_caja = teclado.nextDouble();
+        int dt_caja=(int)((int)(lt%15)*prdt_caja);
+
+        //Total
+        int total =(bulto+caja+dt_caja+dt_bulto);
+        System.out.println("PRECIO TOTAL");
+        System.out.println(total);
+
+
+
+
+
+    }
+
+
+}
